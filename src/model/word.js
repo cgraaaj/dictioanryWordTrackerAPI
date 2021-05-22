b2b23765d4651c2a1b50e3124c506f078da49c59
cgraaaj <cgrvel@gemail.com>
2021-05-22 23:16:31 +0530

    intial commit

---
+++ src/model/word.js
@@ -0,0 +1,88 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const { composeWithMongoose } = require("graphql-compose-mongoose");
+const timestamps = require("mongoose-timestamp");
+const { BookTC } = require("./book");
+const { UserTC } = require("./user");
+
+const WordSchema = new Schema(
+  {
+    search_word: {
+      type: String,
+      required: true,
+    },
+    audio_url: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    definitions: {
+      type: [String],
+      trim: true,
+      required: true,
+    },
+    userIds: [
+      {
+        userId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+        },
+        bookIds: [
+          {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Book",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    collection: "words",
+  }
+);
+
+const customizationOptions = {};
+WordSchema.plugin(timestamps);
+const WordTC = composeWithMongoose(
+  mongoose.model("Word", WordSchema),
+  customizationOptions
+);
+
+WordTC.addRelation("users", {
+  resolver: () => UserTC.getResolver("findMany"),
+  prepareArgs: {
+    filter: (source) => {
+      const userIds = source.userIds.map((user) => user.userId);
+      return {
+        _operators: {
+          _id: {
+            in: userIds || [],
+          },
+        },
+      };
+    },
+  },
+  projection: { userIds: true },
+});
+
+WordTC.addRelation("books", {
+  resolver: () => BookTC.getResolver("findMany"),
+  prepareArgs: {
+    filter: (source) => {
+      const bookIds = source.userIds.reduce(
+        (array, user) => array.concat(user.bookIds),
+        []
+      );
+      return {
+        _operators: {
+          _id: {
+            in: bookIds || [],
+          },
+        },
+      };
+    },
+  },
+  projection: { userIds: true },
+});
+
+module.exports = { Word: mongoose.model("Word", WordSchema), WordTC };
---
+++ src/server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const mongoose = require("mongoose");
+// const { graphqlHTTP } = require("express-graphql");
+const { ApolloServer } = require("apollo-server-express");
+const graphqlSchema = require("./schemas/index");
+// const logger = require("./core/logger");
+
+// const extensions = ({ context }) => {
+//   return {
+//     runTime: Date.now() - context.startTime,
+//   };
+// };
+
+const app = express();
+// app.use(logger);
+const server = new ApolloServer({
+  schema: graphqlSchema,
+  cors: true,
+  path: "/",
+});
+server.applyMiddleware({ app, path: "/", cors: true });
+
+app.listen(5000, async () => {
+  console.log("server is running ", 5000);
+  await mongoose.connect(
+    "mongodb+srv://velan_mongodb:Raju6713@cluster0.64cqd.mongodb.net/wordTracker?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  );
+});
+
+mongoose.connection.on(
+  "error",
+  console.error.bind(console, "MongoDB connection error:")
+);
+// app.use(
+//   "/graphql",
+//   graphqlHTTP((request) => {
+//     return {
+//       context: { startTime: Date.now() },
+//       graphiql: true,
+//       schema: graphqlSchema,
+//       extensions,
+//     };
+//   })
+// );
